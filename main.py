03e0e64b0e69819877e27527e05fc43353827b29
Erik Pohl <epohl@dmigs.com>
2025-01-13 17:15:28 -0500

    added leveraged an  eventbridged lambda function to add elements to dynamodb table when json is uploaded into the s3 bucket

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import boto3
 import json
 
 AWS_BUCKET_EAP = 'first-aws-bucket-1eap'
+AWS_BUCKET_EAP = 'udemys3bucket123'
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -28,15 +29,18 @@ if __name__ == '__main__':
     #     CopySource="first-aws-bucket-1eap/KilroySchematic.svg"
     # )
 
-    s3_resource.Object(AWS_BUCKET_EAP, 'KilroySchematic2.svg').delete()
+    # s3_resource.Object(AWS_BUCKET_EAP, 'KilroySchematic.svg').delete()
 
     pythonusecase = s3_resource.Bucket(name=AWS_BUCKET_EAP)
     for object in pythonusecase.objects.all():
         print(object.key)
 
-    try:
-        bucket = s3_resource.Bucket(AWS_BUCKET_EAP)
-        # bucket.objects.all().delete()
-        s3_resource.Bucket(AWS_BUCKET_EAP).delete()
-    except Exception as e:
-        print(e)
+    s3_resource.Object(AWS_BUCKET_EAP, 'DynamoDB_Samplefile.json'). \
+        upload_file(Filename='C:\\Users\\epohl\\projects\\s3bucket_playground\\DynamoDB_Samplefile.json')
+
+    # try:
+    #     bucket = s3_resource.Bucket(AWS_BUCKET_EAP)
+    #     # bucket.objects.all().delete()
+    #     s3_resource.Bucket(AWS_BUCKET_EAP).delete()
+    # except Exception as e:
+    #     print(e)
